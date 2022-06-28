7f8612a1451587deb21baf4748fabf1b9cefdb0a
Bey Hao Yun <beyhy94@gmail.com>
2022-06-28 23:34:22 +0800

    :hammer: Added env shebang for python3. Formatted script call to that of default main function calling. To improve on feeding of new IP.

--- droidcam.py
+++ droidcam.py
@@ -1,31 +1,41 @@
-import numpy as np
+#!/usr/bin/env python3
+
 import cv2
+import numpy as np
 
-http = 'http://'
-ip_address = '192.168.1.138'
-url =  http + ip_address + ':4747/mjpegfeed?640x480'
-# Opening video stream of ip camera via its url
-cap = cv2.VideoCapture(url)
+HTTP = 'http://'
+IP_ADDRESS = '192.168.1.138'
+URL =  HTTP + IP_ADDRESS + ':4747/mjpegfeed?640x480'
 
-# Corrective actions printed in the even of failed connection.
-if cap.isOpened() is not True:
-    print ('Not opened.')
-    print ('Please ensure the following:')
-    print ('1. DroidCam is not running in your browser.')
-    print ('2. The IP address given is correct.')
 
-# Connection successful. Proceeding to display video stream.
-while cap.isOpened() is True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+def main():
+    print("[ droidcam.py ] - Initializing...")
 
-    # Turning your frames into grayscale
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # Opening video stream of ip camera via its url
+    cap = cv2.VideoCapture(URL)
 
-    cv2.imshow('frame',frame)
-    # cv2.imshow('gray',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Corrective actions printed in the even of failed connection.
+    if cap.isOpened() is not True:
+        print ('Not opened.')
+        print ('Please ensure the following:')
+        print ('1. DroidCam is not running in your browser.')
+        print ('2. The IP address given is correct.')
 
-cap.release()
-cv2.destroyAllWindows()
+    # Connection successful. Proceeding to display video stream.
+    while cap.isOpened() is True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Turning your frames into grayscale
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('frame',frame)
+        # cv2.imshow('gray',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__=='__main__':
+    main()
